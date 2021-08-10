121d33458f5745e867e50970d8778e35d177ef11
Krzysztof Ćwieka <cwieka.krzysztof@gmail.com>
2021-08-10 14:58:22 +0200

    Update settings.py

--- Spotify/settings.py
+++ Spotify/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-C:\Users\Comarch\Documents\DjangoProjects\Spotify
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = open(r"C:\Users\Comarch\Documents\DjangoProjects\Spotify\key.txt").read()
 
