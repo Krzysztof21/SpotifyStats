6c74a8a4ffd3dcf7ac574d37699d11d768351a78
Krzysztof Ćwieka <cwieka.krzysztof@gmail.com>
2021-08-10 19:06:33 +0200

    Secret key arrangements

--- spotify_stats/urls.py
+++ spotify_stats/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'spotify_stats'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:stream_id>/', views.detail, name='detail')
--- spotify_stats/views.py
+++ spotify_stats/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse, Http404
 from django.template import loader
@@ -15,10 +15,7 @@ def index(request):
 
 
 def detail(request, stream_id):
-    try:
-        stream = Stream.objects.get(pk=stream_id)
-    except Stream.DoesNotExist:
-        raise Http404('Stream does not exist')
+    stream = get_object_or_404(Stream, pk=stream_id)
     return render(request, 'spotify_stats/detail.html', {'stream': stream})
 
 
