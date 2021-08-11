3b91b2418a23af0218845e0728022e13fb297b1e
Krzysztof Ćwieka <cwieka.krzysztof@gmail.com>
2021-08-11 21:25:44 +0200

    Implemented total no of streams for track

--- spotify_stats/views.py
+++ spotify_stats/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse, Http404
 from django.template import loader
+from django.db.models import Count
 
-from .models import Stream
+from .models import Stream, Track
 
 
 def index(request):
@@ -15,11 +16,25 @@ def index(request):
 
 
 def detail(request, stream_id):
-    stream = get_object_or_404(Stream, pk=stream_id)
-    return render(request, 'spotify_stats/detail.html', {'stream': stream})
+    stream = Stream.objects.filter(id=stream_id).first()
+    if stream is None:
+        raise Http404("Stream with this ID does not exist")
+    track = Track.objects.filter(stream__id=stream_id).first()
+    return render(request, 'spotify_stats/detail.html', {'stream': stream, 'track': track})
 
 
 def more_detail(request, track_id):
     response = "You're looking at the more details of track %s."
     return HttpResponse(response % track_id)
 
+
+def basic_stats(request, track_id):
+    track = Track.objects.filter(id=track_id).first()
+    # no of streams of this track total
+    total = Stream.objects.filter(track_id=track_id).aggregate(Count('id'))
+    # no of streams of this track last week
+    # total time listened
+    return render(request, 'spotify_stats/basic_stats.html', {'track': track, 'total': total})
+
+# write a form view where you can enter time range
+# and on the next page youll get 5 most listened tracks
