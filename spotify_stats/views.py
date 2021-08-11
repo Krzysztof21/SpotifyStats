0b6fcd1c2dbc35efc5ae4c0c94909f4772d8d4b9
Krzysztof Ćwieka <cwieka.krzysztof@gmail.com>
2021-08-11 21:25:44 +0200

    Learning QuerySet

--- spotify_stats/views.py
+++ spotify_stats/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import loader
 
-from .models import Stream
+from .models import Stream, Track
 
 
 def index(request):
@@ -15,7 +15,15 @@ def index(request):
 
 
 def detail(request, stream_id):
-    stream = get_object_or_404(Stream, pk=stream_id)
+    #stream = get_object_or_404(Stream, pk=stream_id)
+    #stream = Stream.objects.filter(id=stream_id).prefetch_related('track__id_set','track_set')
+    #stream = Stream.objects.select_related('track__track_name').get(id=stream_id)
+    stream = Stream.objects.filter(id=stream_id).first()
+    track = list(Stream.objects.filter(track__id=stream_id).first())
+    track_name = stream.ms_played
+    #artist_name
+    #album_name
+    #min_played
     return render(request, 'spotify_stats/detail.html', {'stream': stream})
 
 
@@ -23,3 +31,5 @@ def more_detail(request, track_id):
     response = "You're looking at the more details of track %s."
     return HttpResponse(response % track_id)
 
+# write a form view where you can enter time range
+# and on the next page youll get 5 most listened tracks
