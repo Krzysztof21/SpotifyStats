1f2b5294d7ccccca4f3f8fc64c3cb8d34405e8ae
Krzysztof Ćwieka <cwieka.krzysztof@gmail.com>
2021-08-15 11:54:09 +0200

    pick_date view updated to have include_podcasts checkbox; most_listened receives this boolean

--- spotify_stats/forms.py
+++ spotify_stats/forms.py
@@ -4,4 +4,5 @@ from django import forms
 class DateForm(forms.Form):
     start_date = forms.DateTimeField(label='Start date')
     end_date = forms.DateTimeField(label='End date')
+    include_podcasts = forms.BooleanField(widget=forms.CheckboxInput(), label='Include podcasts')
 
--- spotify_stats/utils.py
+++ spotify_stats/utils.py
@@ -1,5 +1,5 @@
 def convert_millis(millis):
     seconds = int((millis/1000) % 60)
     minutes = int((millis/(1000*60)) % 60)
-    hours = int((millis/(1000*60*60)) % 24)
+    hours = int(millis/(1000*60*60))
     return hours, minutes, seconds
