e2f03e9fadb371fc0735c7dc8ac4125544b86d79
Krzysztof Ćwieka <cwieka.krzysztof@gmail.com>
2021-08-16 21:26:29 +0200

    most_listened refactored - added options for limit and order, changed to raw SQLite

--- spotify_stats/forms.py
+++ spotify_stats/forms.py
@@ -4,5 +4,11 @@ from django import forms
 class DateForm(forms.Form):
     start_date = forms.DateTimeField(label='Start date')
     end_date = forms.DateTimeField(label='End date')
-    include_podcasts = forms.BooleanField(widget=forms.CheckboxInput(), label='Include podcasts')
+    include_podcasts = forms.BooleanField(widget=forms.CheckboxInput(), label='Include podcasts', required=False)
+    limit = forms.IntegerField(min_value=5, initial=5)
+    order_choices = [
+        ('total_time', 'Total time listened'),
+        ('no_streams', 'Number of streams')
+    ]
+    order = forms.CharField(label='Choose order', widget=forms.Select(choices=order_choices))
 
--- spotify_stats/utils.py
+++ spotify_stats/utils.py
@@ -1,5 +1,7 @@
 def convert_millis(millis):
     seconds = int((millis/1000) % 60)
+    seconds = f'0{seconds}' if seconds < 10 else str(seconds)
     minutes = int((millis/(1000*60)) % 60)
+    minutes = f'0{minutes}' if minutes < 10 else str(minutes)
     hours = int(millis/(1000*60*60))
     return hours, minutes, seconds
