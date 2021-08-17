f2b5e1f1384d4833b902a622c97f08a561e93666
Krzysztof Ćwieka <cwieka.krzysztof@gmail.com>
2021-08-17 21:02:00 +0200

    Date validation - DONE

--- spotify_stats/forms.py
+++ spotify_stats/forms.py
@@ -1,6 +1,5 @@
 import datetime as dt
 
-
 from django import forms
 from django.core.exceptions import ValidationError
 
@@ -16,13 +15,6 @@ class DateForm(forms.Form):
     ]
     order = forms.CharField(label='Choose order', widget=forms.Select(choices=order_choices))
 
-    def check_date_before_present(self, date):
-        frmt = "%Y-%m-%d %H:%M:%S%z"
-        formatted_date = dt.datetime.strptime(date, frmt)
-        if formatted_date > dt.datetime.now():
-            raise ValidationError("Date must be in the past")
-        return date
-
     def clean_start_date(self):
         return self.check_date_before_present(self.cleaned_data['start_date'])
 
@@ -31,7 +23,16 @@ class DateForm(forms.Form):
 
     def clean(self):
         cleaned_data = super().clean()
+        if any(self.errors):
+            return self.errors
         start_date = cleaned_data.get("start_date")
         end_date = cleaned_data.get("end_date")
         if end_date < start_date:
             raise ValidationError("Start date must be earlier than end date")
+
+    @staticmethod
+    def check_date_before_present(date):
+        now = dt.datetime.now(dt.timezone.utc).astimezone()
+        if date > now:
+            raise forms.ValidationError("Date must be in the past")
+        return date
