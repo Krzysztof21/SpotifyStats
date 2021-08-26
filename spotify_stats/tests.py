9bd3f07599b6557dd7bb3c00390220244b558b8d
Krzysztof21 <cwieka.krzysztof@gmail.com>
2021-08-26 17:09:42 +0200

    pick_date and tests refactored, StreamManager created

--- spotify_stats/tests.py
+++ spotify_stats/tests.py
@@ -1,15 +1,10 @@
 import pytest
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 from django.urls import reverse
 
 from .utils import milliseconds_to_hh_mm_ss
 from .models import Stream, Track
-
-from django.urls.exceptions import NoReverseMatch
-
-
-
 
 
 class TestMillisecondsConverter:
@@ -41,7 +36,7 @@ class TestModels:
     @pytest.fixture
     def stream_1(self, db, track_1):
         return Stream.objects.create(
-            end_time='2020-01-16 23:30',
+            end_time='2020-01-16 23:30+01:00',
             ms_played='38089',
             track=track_1
         )
@@ -63,20 +58,20 @@ class TestModels:
 class TestBasicStats:
     @pytest.fixture
     def db_test_version(self, db):
-        date_last_week = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d %H:%M')
+        date_last_week = (datetime.now(timezone.utc) - timedelta(days=1)).strftime('%Y-%m-%d %H:%M%z')
         track = Track.objects.create(
             track_name='Ethnicolor - Remastered', album_name='Zoolook', artist_name='Jean-Michel Jarre'
         )
         track.save()
         data = [
-            Stream(end_time='2020-01-16 23:30', ms_played='300000', track=track),
-            Stream(end_time='2020-02-16 23:30', ms_played='300000', track=track),
-            Stream(end_time='2020-03-16 23:30', ms_played='300000', track=track),
+            Stream(end_time='2020-01-16 23:30+01:00', ms_played='300000', track=track),
+            Stream(end_time='2020-02-16 23:30+01:00', ms_played='300000', track=track),
+            Stream(end_time='2020-03-16 23:30+01:00', ms_played='300000', track=track),
             Stream(end_time=date_last_week,     ms_played='300000', track=track)
         ]
         Stream.objects.bulk_create(data)
 
-    def test(self, db_test_version, client):
+    def test_existing(self, db_test_version, client):
         response = client.get(reverse('spotify_stats:basic_stats', args=[1]))
         assert response.status_code == 200
         response_content = response.content.decode()
@@ -86,3 +81,6 @@ class TestBasicStats:
         assert "0:20:00" in response_content
         assert "1" in response_content
 
+    def test_nonexistent(self, client):
+        response = client.get(reverse('spotify_stats:basic_stats', args=[3]))
+        assert response.status_code == 404
