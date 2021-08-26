a1b843501195084240fd2eeb97b022a27794f4f2
Krzysztof21 <cwieka.krzysztof@gmail.com>
2021-08-26 17:09:42 +0200

    pick_date and tests refactored, StreamManager created

--- spotify_stats/models.py
+++ spotify_stats/models.py
@@ -1,7 +1,29 @@
 from django.db import models
+from django.db.models import Count, Sum
+
+from datetime import datetime, timedelta, timezone
+from .utils import milliseconds_to_hh_mm_ss
 
 
-# Create your models here.
+class StreamManager(models.Manager):
+    def get_total_streams(self, track_id):
+        return self._get_streams_for_track(track_id).aggregate(Count('id'))['id__count']
+
+    def get_total_time_listened(self, track_id):
+        time_tuple = milliseconds_to_hh_mm_ss(
+            self._get_streams_for_track(track_id).aggregate(Sum('ms_played'))['ms_played__sum'])
+        return f'{time_tuple[0]}:{time_tuple[1]}:{time_tuple[2]}'
+
+    def get_number_last_week_streams(self, track_id):
+        today = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M%z')
+        week_ago = (datetime.now(timezone.utc) - timedelta(days=7)).strftime('%Y-%m-%d %H:%M%z')
+        streams = self._get_streams_for_track(track_id).filter(end_time__gte=week_ago, end_time__lte=today)
+        return streams.aggregate(Count('id'))['id__count']
+
+    def _get_streams_for_track(self, track_id):
+        return self.filter(track_id=track_id)
+
+
 class Track(models.Model):
     track_name = models.TextField(blank=True, null=True)
     album_name = models.TextField(blank=True, null=True)
@@ -16,6 +38,8 @@ class Track(models.Model):
 
 
 class Stream(models.Model):
+    objects = StreamManager()
+
     end_time = models.DateTimeField()
     ms_played = models.IntegerField()
     track = models.ForeignKey(Track, on_delete=models.CASCADE)
@@ -25,5 +49,8 @@ class Stream(models.Model):
         db_table = 'stream'
 
     def __str__(self):
-        return str(self.end_time)
+        return str(self.track)
+
+
+
 
--- spotify_stats/views.py
+++ spotify_stats/views.py
@@ -1,10 +1,9 @@
-from datetime import datetime, timedelta
+from urllib.parse import urlencode
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect, Http404
 from django.views import View
-from django.db.models import Count, Sum
 
 from .models import Stream, Track
 from .forms import DateForm
@@ -32,15 +31,15 @@ def pick_date(request):
     if request.method == 'POST':
         form = DateForm(request.POST)
         if form.is_valid():
-            frmt = '%Y-%m-%d %H:%M'
-            start = form.cleaned_data["start_date"].strftime(frmt)
-            end = form.cleaned_data["end_date"].strftime(frmt)
-            include_podcasts = form.cleaned_data['include_podcasts']
-            limit = form.cleaned_data['limit']
-            order = form.cleaned_data['order']
-            url = f'/spotify_stats/most_listened?start_time={start}&end_time={end}&' \
-                + f'include_podcasts={include_podcasts}&limit={limit}&order={order}'
-            return HttpResponseRedirect(url)
+            base_url = reverse('spotify_stats:most_listened')
+            query_string = urlencode({
+                'start_time': form.cleaned_data["start_date"].strftime('%Y-%m-%d %H:%M%z'),
+                'end_time':form.cleaned_data["end_date"].strftime('%Y-%m-%d %H:%M%z'),
+                'include_podcasts': form.cleaned_data['include_podcasts'],
+                'limit': form.cleaned_data['limit'],
+                'order': form.cleaned_data['order']})
+            url = '{}?{}'.format(base_url, query_string)
+            return redirect(url)
     else:
         form = DateForm()
     return render(request, 'spotify_stats/pick_date.html', {'form': form})
@@ -72,11 +71,11 @@ def most_listened(request):
 
 class BasicStats(View):
     def get(self, request, track_id):
-        track = Track.objects.filter(id=track_id).first()
+        track = get_object_or_404(Track, pk=track_id)
         stats = {
-            'total_streams': self.get_total_streams(track_id),
-            'total_time_listened': self.get_total_time_listened(track_id),
-            'last_week_streams': self.get_number_last_week_streams(track_id)
+            'total_streams': Stream.objects.get_total_streams(track_id),
+            'total_time_listened': Stream.objects.get_total_time_listened(track_id),
+            'last_week_streams': Stream.objects.get_number_last_week_streams(track_id)
         }
         context = {
             'track': track,
@@ -84,20 +83,3 @@ class BasicStats(View):
         }
         return render(request, 'spotify_stats/basic_stats.html', context)
 
-    def get_total_streams(self, track_id):
-        return self._get_streams_for_track(track_id).aggregate(Count('id'))['id__count']
-
-    def get_total_time_listened(self, track_id):
-        time_tuple = milliseconds_to_hh_mm_ss(
-            self._get_streams_for_track(track_id).aggregate(Sum('ms_played'))['ms_played__sum'])
-        return f'{time_tuple[0]}:{time_tuple[1]}:{time_tuple[2]}'
-
-    def get_number_last_week_streams(self, track_id):
-        today = datetime.today().strftime('%Y-%m-%d %H:%M')
-        week_ago = (datetime.today() - timedelta(days=7)).strftime('%Y-%m-%d %H:%M')
-        streams = self._get_streams_for_track(track_id).filter(end_time__gte=week_ago, end_time__lte=today)
-        return streams.aggregate(Count('id'))['id__count']
-
-    @staticmethod
-    def _get_streams_for_track(self, track_id):
-        return Stream.objects.filter(track_id=track_id)
