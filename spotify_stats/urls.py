de11b35a726a368b07f977d7852dcb0a2ed11d8f
Krzysztof21 <cwieka.krzysztof@gmail.com>
2021-09-03 11:47:55 +0200

    most_listened and pick_date refactored into CBV

--- spotify_stats/urls.py
+++ spotify_stats/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('streams/<int:pk>/', views.StreamDetailView.as_view(), name='stream_detail'),
     path('tracks/<int:pk>/', views.TrackDetailView.as_view(), name='track_detail'),
     path('basic_stats/<int:track_id>/', BasicStats.as_view(), name='basic_stats'),
-    path('pick_date/', views.pick_date, name='pick_date'),
-    path('most_listened/', views.most_listened, name='most_listened'),
+    path('pick_date/', views.PickDateFormView.as_view(), name='pick_date'),
+    path('most_listened/', views.MostListenedListView.as_view(), name='most_listened'),
 ]
--- spotify_stats/views.py
+++ spotify_stats/views.py
@@ -2,8 +2,10 @@ from urllib.parse import urlencode
 
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 
-from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.views import View, generic
+from django.views.generic.edit import FormView
+from django.views.generic.list import ListView
 
 from .models import Stream, Track
 from .forms import DateForm
@@ -24,50 +26,56 @@ class StreamDetailView(generic.DetailView):
     template_name = 'spotify_stats/stream_detail.html'
 
 
-def pick_date(request):
-    if request.method == 'POST':
-        form = DateForm(request.POST)
-        if form.is_valid():
-            base_url = reverse('spotify_stats:most_listened')
-            query_string = urlencode({
-                'start_time': form.cleaned_data["start_date"].strftime('%Y-%m-%d %H:%M%z'),
-                'end_time': form.cleaned_data["end_date"].strftime('%Y-%m-%d %H:%M%z'),
-                'include_podcasts': form.cleaned_data['include_podcasts'],
-                'limit': form.cleaned_data['limit'],
-                'order': form.cleaned_data['order']})
-            url = '{}?{}'.format(base_url, query_string)
-            return redirect(url)
-    else:
-        form = DateForm()
-    return render(request, 'spotify_stats/pick_date.html', {'form': form})
+class PickDateFormView(FormView):
+    template_name = 'spotify_stats/pick_date.html'
+    form_class = DateForm
+
+    def get_success_url(self, form):
+        base_url = reverse('spotify_stats:most_listened')
+        query_string = urlencode({
+            'start_time': form.cleaned_data["start_date"].strftime('%Y-%m-%d %H:%M%z'),
+            'end_time': form.cleaned_data["end_date"].strftime('%Y-%m-%d %H:%M%z'),
+            'include_podcasts': form.cleaned_data['include_podcasts'],
+            'limit': form.cleaned_data['limit'],
+            'order': form.cleaned_data['order']})
+        return '{}?{}'.format(base_url, query_string)
+
+    def form_valid(self, form):
+        return HttpResponseRedirect(self.get_success_url(form))
 
 
 # TODO: refactor
-def most_listened(request):
-    start_time = request.GET.get('start_time')[:10]
-    end_time = request.GET.get('end_time')[:10]
-    include_podcasts = request.GET.get('include_podcasts')
-    podcast_filter = '' if include_podcasts == 'True' else "WHERE t.album_name IS NOT 'None'"
-    limit = request.GET.get('limit')
-    order = request.GET.get('order')
-    query = f'''
-        SELECT s.id, s.track_id, t.track_name, s.total_time, t.album_name, s.no_streams FROM
-            (SELECT sum(ms_played) AS total_time, count(id) AS no_streams, id, track_id FROM stream
-                WHERE end_time >= "{start_time}" AND end_time < "{end_time}" GROUP BY track_id) AS s
-            JOIN track AS t ON s.track_id=t.id
-            {podcast_filter}
-            ORDER BY s.{order} DESC
-            LIMIT {limit};  
-        '''
-    streams = list(Stream.objects.raw(query))
-    for s in streams:
-        t = milliseconds_to_hh_mm_ss(s.total_time)
-        s.total_time = f'{t[0]}:{t[1]}:{t[2]}'
-    return render(request, 'spotify_stats/most_listened.html', {'streams': streams})
+class MostListenedListView(ListView):
+    template_name = 'spotify_stats/most_listened.html'
+    context_object_name = 'streams'
+
+    def get_queryset(self):
+        start_time = self.request.GET['start_time'][:10]
+        end_time = self.request.GET['end_time'][:10]
+        include_podcasts = self.request.GET['include_podcasts']
+        podcast_filter = '' if include_podcasts == 'True' else "WHERE t.album_name IS NOT 'None'"
+        limit = self.request.GET['limit']
+        order = self.request.GET['order']
+        query = f'''
+                SELECT s.id, s.track_id, t.track_name, s.total_time, t.album_name, s.no_streams FROM
+                    (SELECT sum(ms_played) AS total_time, count(id) AS no_streams, id, track_id FROM stream
+                        WHERE end_time >= "{start_time}" AND end_time < "{end_time}" GROUP BY track_id) AS s
+                    JOIN track AS t ON s.track_id=t.id
+                    {podcast_filter}
+                    ORDER BY s.{order} DESC
+                    LIMIT {limit};  
+                '''
+        streams = list(Stream.objects.raw(query))
+        for s in streams:
+            t = milliseconds_to_hh_mm_ss(s.total_time)
+            s.total_time = f'{t[0]}:{t[1]}:{t[2]}'
+        return streams
 
 
 class BasicStats(View):
-    def get(self, request, track_id):
+
+    @staticmethod
+    def get(request, track_id):
         track = get_object_or_404(Track, pk=track_id)
         stats = {
             'total_streams': Stream.objects.get_total_streams(track_id),
@@ -79,4 +87,3 @@ class BasicStats(View):
             'stats': stats
         }
         return render(request, 'spotify_stats/basic_stats.html', context)
-
